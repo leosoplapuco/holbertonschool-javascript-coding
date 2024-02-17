1b1d1c836fa850a7aad0150e7c130bd9f2391c55
leosoplapuco <leosoplapuco29@gmail.com>
2024-02-17 13:19:04 -0500

    Fixed Up: 4-starwars_count - Correct documentation 03'

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -12,9 +12,9 @@ request(reqURL, function (error, response, body) {
     const results = jso['results'];
     let count = 0;
     for (let i = 0; i < results.length; i++) {
-      let chars = (results[i]['characters']);
+      const chars = (results[i]['characters']);
       for (let j = 0; j < chars.length; j++) {
-        let check18 = chars[j].endsWith('18/');
+        const check18 = chars[j].endsWith('18/');
         if (check18) {
           count++;
         }
