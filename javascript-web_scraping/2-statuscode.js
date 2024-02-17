914c6886dbf5295c6d9b0160fe5667b59a1e11ca
leosoplapuco <leosoplapuco29@gmail.com>
2024-02-17 12:56:07 -0500

    Fixed up: 2-statuscode - Correct documentation

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -8,4 +8,3 @@ request(args[2], function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
   } else console.log('code:', response && response.statusCode);
 });
-
